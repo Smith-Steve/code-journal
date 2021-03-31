04d60a16756a89f2f452dfe3ebd0b02be8a47a95
Steve-A-Dore <Steve.A.Dore@outlook.com>
2021-03-30 20:59:44 -0700

    steve commit per cody.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,17 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousToDoJSON = localStorage.getItem('code-journal');
+
+if (previousToDoJSON !== null) {
+  data = JSON.parse(previousToDoJSON);
+}
+
+// event listener beforeUnload
+window.addEventListener('beforeunload', function (event) {
+  // stringify data
+  var transformToJSON = JSON.stringify(data);
+  localStorage.setItem('code-journal', transformToJSON);
+  // localStorage.setItem('thing', JSON data)
+});
--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 /* global data */
 /* exported data */
+
+var $photoElement = document.getElementById('photo_url');
+var $form = document.getElementById('data-entry-form');
+
+function handleImage(event) {
+  var imageElement = document.querySelector('img');
+  var imageUrl = 'images/placeholder-image-square.jpg';
+  if ($photoElement.value !== null) {
+    imageUrl = $photoElement.value;
+    imageElement.setAttribute('src', imageUrl);
+  } else {
+    imageElement.setAttribute('src', imageUrl);
+  }
+}
+
+function handleForm(event) {
+  event.preventDefault();
+  var userInput = {
+    title: $form.elements.title.value,
+    photo: $form.elements.photo_url.value,
+    notes: $form.elements.notes.value,
+    entryID: data.nextEntryId
+  }; // put the form's input values into a new object. Add the nextEntryID to the object.
+  data.nextEntryId++; // Increment the nextEntryId on the data model.
+  data.entries.unshift(userInput); // The task list says, 'prepend' which means - to add to the front of.
+  // data.entries.nextEntry++;
+  $form.reset();
+}
+
+$photoElement.addEventListener('input', handleImage);
+$form.addEventListener('submit', handleForm);
